ecab3f25af914d28cfe467290421dc85bf1d6b78
bevexed <547551650@qq.com>
2019-02-27 17:50:39 +0800

    初始化

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/wx', function(req, res, next) {
+  //微信得到返回后会通过你的认证
+  var query = req.query;
+  var echostr = query.echostr;
+  res.send(echostr);
+});
+
+module.exports = router;
